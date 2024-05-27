eb7b0127349833b0eac8ac0c40426e8590ba9803
KushSingh93 <131660344+KushSingh93@users.noreply.github.com>
2024-05-26 23:25:34 -0700

    Add files via upload

---
+++ precache-manifest.dc1ec59d9cd5b9f4eb2253a4a307b522.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7317ec8f82f67bf67deb31995bc1872c",
+    "url": "/prime_algorithms/index.html"
+  },
+  {
+    "revision": "0334ca708821fca85dfc",
+    "url": "/prime_algorithms/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6d07363a4b375d30800f",
+    "url": "/prime_algorithms/static/js/2.644a093a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/prime_algorithms/static/js/2.644a093a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0334ca708821fca85dfc",
+    "url": "/prime_algorithms/static/js/main.407060a9.chunk.js"
+  },
+  {
+    "revision": "ed48f28a56267d666786",
+    "url": "/prime_algorithms/static/js/runtime-main.389a0f51.js"
+  }
+]);
